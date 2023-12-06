ef8e85f26d293417865eb1d27f58185a9641b501
StevenShumPitt <134329857+StevenShumPitt@users.noreply.github.com>
2023-12-06 10:50:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from HousePyEPR import *
 
 analysis=House_PyEPR(HFSSproject_path=r'C:\Users\CHS357\Desktop\Desgin\HFSS\Shoebox',
                      HFSSproject_name=r'YaleCav',
-                     HFSSdesign_name=r'BigChip_shift',
-                     iter_num=r'_87',
+                     HFSSdesign_name=r'SmallChip_shift1',
+                     iter_num=r'_01',
                      savingdirectory=r'C:\Users\CHS357\Documents\GitHub\pyEPR\chs357\project_data',
-                     savingfilename=r'BigChip')
+                     savingfilename=r'SmallChip_Update')
 
 analysis.save()
 analysis.run()
